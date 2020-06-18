f8e30146365f8d4bed6bb3eb0b7f8a535ae1b3b3
5+1 <59787082+langonginc@users.noreply.github.com>
2020-06-18 21:12:24 +0800

    PUSH-UP

--- install.sh
+++ install.sh
@@ -8,16 +8,30 @@ wget --version || (echo "Please Install Wget." && exit 1)
 echo 
 echo
 echo
-echo "Allow you to install!"
+echo " >> Allow you to install!"
+echo " >> Compile main program..."
 g++ main.cpp -o LanGong
 echo
 cd ~/
+echo " >> Making DIR..."
 mkdir LanGongFile
 cd  LanGongFile
+echo " >> Creating log..."
 touch .lanrc
 echo "Made by LanGongINC," > ~/.config/LanFilePackage.log
 echo -n "LanFilePackage is install at " >> ~/.config/LanFilePackage.log
 date -u >> ~/.config/LanFilePackage.log 
+
+echo " >> Creat alias..."
+echo "
+cd ~/LanFilePackage 
+./LanGong 
+" > .lfpa
+$als="alias lfp='bash ~/.lfpa'"
+echo $als >> ~/.zshrc
+echo $als >> ~/.bashrc
+echo $als >> ~/.fishrc
+
 echo
-echo
+echo " >> Install seccussfully."
 exit 0
