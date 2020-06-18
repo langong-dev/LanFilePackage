31804cff77691eec483e00293c3c613768e1aaec
5+1 <59787082+langonginc@users.noreply.github.com>
2020-06-18 21:32:41 +0800

    PUSH-UP

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ echo -n "LanFilePackage is install at " >> ~/.config/LanFilePackage.log
 date -u >> ~/.config/LanFilePackage.log 
 
 echo " >> Creat alias..."
-echo "cd ~/LanFilePackage && ./LanGong " > ~/.lfpa
+echo "cd ~/LanFilePackage && ./LanGong \$1 \$2 \$3 \$4 \$5" > ~/.lfpa
 als="alias lfp=\"bash ~/.lfpa\""
 echo $als >> ~/.zshrc
 echo $als >> ~/.bashrc
